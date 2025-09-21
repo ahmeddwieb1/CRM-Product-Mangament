9fe93fc228e9b8c116aa0a660f496f567eab9067
ahmeddwieb <ahmeddwieb713@gmail.com>
2025-09-22 00:58:27 +0200

    fix issuse CORS in swagger

--- src/main/java/org/elmorshedy/security/OpenApiConfig.java
+++ src/main/java/org/elmorshedy/security/OpenApiConfig.java
@@ -4,8 +4,12 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
 
 @Configuration
 public class OpenApiConfig {
@@ -13,6 +17,9 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://crm-product-mangament-production.up.railway.app")
+                ))
                 .components(new Components()
                         .addSecuritySchemes("bearer-jwt",
                                 new SecurityScheme()
@@ -21,4 +28,5 @@ public class OpenApiConfig {
                                         .bearerFormat("JWT")))
                 .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"));
     }
+
 }
